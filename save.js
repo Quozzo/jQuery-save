3003712012f8289e9f66ec32460cd6c403956853
Quozzo <quozzo@hotmail.com>
2016-05-27 03:42:24 +0100

    More ES6 goodness also optimised some loops.

--- save.js
+++ save.js
@@ -2,8 +2,7 @@
 
   //find the root||start of the chain.
   let saved = new WeakMap(),
-    root = function() {
-      let root = this.filter(':first');
+    root = function(root) {
       while (root.prevObject.prevObject) {
         root = root.prevObject;
       }
@@ -12,51 +11,33 @@
 
   $.fn.save = function(obj, deep) {
 
-    let r = root.call(this),
-      rootSave = saved.get(r),
-      data = rootSave || new Map();
+    let r = root(this), rootSave = saved.get(r), data = rootSave || new Map(), objStr = typeof obj === "string";
 
     //returns the data
-    if (!obj || typeof obj === "string" || obj === true) {
+    if (!obj || objStr || obj === true) {
       if (!rootSave) return undefined;
-      let map = [],
-        mapping = (typeof obj === "string") ? function(v) {
-          map[map.length] = v[obj];
-        } : function(v) {
-          map[map.length] = v;
-        };
+      let map = [], mapping = objStr ? v => map[map.length] = v[obj] : v => map[map.length] = v;
       rootSave.forEach(mapping);
       //flattens the array to first saved, or last saved if true, true.
       if (obj === true) map = map[(deep ? "reduceRight" : "reduce")]((curr, prev) => $.extend(prev, curr));
       return map;
     } else if (obj instanceof $) {
-      let rO = root.call(obj),
-        objSave = saved.get(rO);
-      if (!objSave) {
-        return this.add(obj);
-      } else if (!rootSave) {
-        saved.set(r, objSave);
-      } else if (rootSave && objSave) {
-        objSave.forEach((v, k) => rootSave.set(k, $.extend(true, rootSave.get(k), v)));
-      }
+      let rO = root(obj), objSave = saved.get(rO);
+      if (!objSave) return this.add(obj);
+      else if (!rootSave) saved.set(r, objSave);
+      else if (rootSave && objSave) objSave.forEach((v, k) => rootSave.set(k, $.extend(true, rootSave.get(k), v)));
       return this.add(obj);
     }
 
     //retrieves and saves the data
     this.each(function() {
-      let info = {},
-        $this = $(this),
-        index;
+      let info = {}, $this = $(this), index;
       if (rootSave) index = rootSave.get(this);
       for (let key in obj) {
-        if (typeof obj[key] === "string") {
-          info[key] = $this[obj[key]]();
-        } else if (typeof obj[key] === "object") {
-          info[key] = $this[obj[key][0]](obj[key][1]);
-        } else if (typeof obj[key] === "function") {
-          let prevData = index ? index[key] : undefined;
-          info[key] = obj[key].call(this, prevData);
-        }
+        let objKey = typeof obj[key];
+        if (objKey === "string") info[key] = $this[obj[key]]();
+        else if (objKey === "object") info[key] = $this[obj[key][0]](obj[key][1]);
+        else if (objKey === "function") info[key] = obj[key].call(this, index ? index[key] : undefined);
       }
       data.set(this, $.extend(true, index, info));
     });
